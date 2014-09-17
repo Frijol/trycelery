5c043596f449a8bc892c5bbb3f8464621ab1f522
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2014-09-17 14:35:55 -0700

    not much here but configured for v2 now

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-var config = require('./.env').config;
+var config = require('./config.json');
 
-console.log(config)
+var request = require('request');
+
+// All requests expect these headers
+var headers = {
+  "Content-Type": "",
+  "Accept": "application/" + config.datatype, //json or xml
+  "Authorization": config.token
+};
+
+var baseURL = "https://api.trycelery.com"; // Base URL
+var URLs = {
+  
+}
+
+// Sample error response
+// {
+//     "meta": {
+//         "code": 409,
+//         "error": {
+//            "code": 409,
+//            "message": "User with email already exists."
+//          }
+//     },
+//     "data": "User with email already exists."
+// }
+
+// Sample response
+// {
+//     "meta": {
+//         "code": 200,
+//         "paging": {
+//             "total": 45,
+//             "count": 10,
+//             "offset": 5,
+//             "limit": 10,
+//             "has_more": true
+//         }
+//     },
+//     "data": [
+//         {
+//             ...
+//         }
+//     ]
+// }
